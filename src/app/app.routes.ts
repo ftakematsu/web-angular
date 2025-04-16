55fdb38898a36d10bb800a2c80528760f553a99a
ftakematsu <ftakematsu>
2025-04-16 08:58:20 -0300

    routes init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+/**
+ * Arquivo de rotas gerais do sistema, que chama as outras rotas
+ */
+export const routes: Routes = [
+    {
+        path: 'home', // O que vai aparecer na URL
+        loadChildren: () => import('./home/routes/home.routes')
+    }
+];
---
+++ src/app/home/routes/home.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "../home.component";
+
+/**
+ * Arquivo de rotas da home.component
+ */
+export default [
+    {
+        path: '',
+        component: HomeComponent
+    }
+] as Routes;
