0d6d02f4a75db499db2db84d8ab9f217c081c9e8
Fabio <ftakematsu@gmail.com>
2025-05-29 20:33:50 -0300

    atualizações de componentes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatButtonModule} from '@angular/material/button';
 import { Router } from '@angular/router';
+import { MycomponentComponent } from '../mypage/mycomponent/mycomponent.component';
 
 @Component({
   selector: 'app-home',
-  imports: [MatProgressSpinnerModule, MatButtonModule],
+  imports: [MatProgressSpinnerModule, MatButtonModule,
+    MycomponentComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/mypage/mypage.component.ts
+++ src/app/mypage/mypage.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
+import { MycomponentComponent } from './mycomponent/mycomponent.component';
+import { SecondComponent } from './second/second.component';
 
 @Component({
   selector: 'app-mypage',
-  imports: [MatInputModule, MatFormFieldModule, FormsModule],
+  imports: [
+    MatInputModule, 
+    MatFormFieldModule, 
+    FormsModule,
+    MycomponentComponent,
+    SecondComponent
+  ],
   templateUrl: './mypage.component.html',
   styleUrl: './mypage.component.css'
 })
