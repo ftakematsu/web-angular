7ade0319ae32be3d60f14ed1f8fdc85a097b8584
Fabio <ftakematsu@gmail.com>
2025-05-29 21:44:30 -0300

    directivas do Angular

--- src/app/mypage/mypage.component.ts
+++ src/app/mypage/mypage.component.ts
@@ -4,6 +4,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import { MycomponentComponent } from './mycomponent/mycomponent.component';
 import { SecondComponent } from './second/second.component';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-mypage',
@@ -12,13 +14,22 @@ import { SecondComponent } from './second/second.component';
     MatFormFieldModule, 
     FormsModule,
     MycomponentComponent,
-    SecondComponent
+    SecondComponent,
+    CommonModule,
+    MatButtonModule
   ],
   templateUrl: './mypage.component.html',
   styleUrl: './mypage.component.css'
 })
 export class MypageComponent {
   nome: string = "";
+
+  lista = [
+    { nome: "Fulano de Tal", idade: 20 },
+    { nome: "Beltrano de Tal", idade: 25 },
+    { nome: "Ciclano de Tal", idade: 35 }
+  ];
+
   enviar() {
     alert("Boa noite " + this.nome);
   }
